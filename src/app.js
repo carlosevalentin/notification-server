d5d597510466fff2ca7f700a7fd9396b97b720c1
Carlos Eduardo <carlos.evalentin@gmail.com>
2025-07-28 20:53:45 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const notificationRoutes = require('./api/routes/notification.routes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(
+  cors({
+    origin: 'http://localhost:4200', // ou '*' se quiser liberar para todos
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
+
 app.use(express.json());
 
 app.use('/api', notificationRoutes);
